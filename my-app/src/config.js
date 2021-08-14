d6b2b6d265ec805f9539c21b2c101de8666e181e
Ariel Bentolila <ariel.bentolila@sap.com>
2021-08-14 17:44:16 +0300

    cleanup

--- my-app/src/config.js
+++ my-app/src/config.js
@@ -2,7 +2,7 @@ export const config = {
     // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
     // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
     // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
-    apiKey: "AIzaSyALqHMEQb4xklCnIqUP_hqbKXEo8u0zx8A",
+    apiKey: "<APIKEY>",
     //databaseURL: "https://react-firebase-baecd.firebaseio.com",
-    projectId: "atpenn-fe837",
+    projectId: "<Project-ID>",
   };
